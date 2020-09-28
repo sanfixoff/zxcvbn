91a17a71c5dce27cd68b83a2d7b9b209571c1a8d
Leonid <tugrew@inbox.ru>
2020-09-28 10:10:37 +0500

    lesson 6

---
+++ clothes-shop-5/assets/js/slickStyle.js
@@ -0,0 +1,13 @@
+
+$( document ).ready(function() {
+   $('.reviews-slider-js').slick({
+		slidesToShow: 1,
+ 	 	slidesToScroll: 1,
+ 	 	autoplay: false,
+ 	 	autoplaySpeed: 3000,
+ 	 	dots: true,
+ 	 	arrows: false,
+ 	 	centerMode: false,
+	});
+});
+	
